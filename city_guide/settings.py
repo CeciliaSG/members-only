7bcdf32d8f82fa78fbfe9bd3ae3ed6845f8c9ffc
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-04-01 13:24:23 +0000

    Add https://*.gitpod.io to CSRF_TRUSTED_ORIGINS

--- city_guide/settings.py
+++ city_guide/settings.py
@@ -30,11 +30,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ceciliasg-membersonly-lgambh2rrdp.ws-eu110.gitpod.io', '.herokuapp.com']
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-ceciliasg-membersonly-lgambh2rrdp.ws-eu110.gitpod.io', "https://*.herokuapp.com"
+CSRF_TRUSTED_ORIGINS = ['https://8000-ceciliasg-membersonly-lgambh2rrdp.ws-eu110.gitpod.io', "https://*.herokuapp.com", "https://*.gitpod.io"
 ]
 
 
