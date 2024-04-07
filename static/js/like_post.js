27b6e62bc6f684a6f5dfee51bb1bfe63e5b39554
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-04-07 20:57:59 +0000

    Fix close function in post_like

--- static/js/like_post.js
+++ static/js/like_post.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
-            console.log("Document ready function reached.");
-            $('#like-button').click(function () {
-                        console.log("Like button clicked.");
-                        let postId = $(this).data('post-id');
-                        console.log("Post ID:", postId);
-                        let csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
-                        console.log("CSRF Token:", csrfToken);
+    console.log("Document ready function reached.");
+    $('#like-button').click(function () {
+        console.log("Like button clicked.");
+        let postId = $(this).data('post-id');
+        console.log("Post ID:", postId);
+        let csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+        console.log("CSRF Token:", csrfToken);
+    });
+});
