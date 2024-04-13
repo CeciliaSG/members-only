cffa8a4dfd39b86209c5dcfd08b41231289cab03
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-04-13 10:54:31 +0000

    Add view and url for filtering and displaying posts for restaurants and bars

--- content_management/urls.py
+++ content_management/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .views import PostListView, post_detail
+from .views import PostListView, post_detail, tag_filter
 from . import views
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('tag_filter/<str:tag>/', views.tag_filter, name='restaurants_bars'),
+    #path('tag_filter/<str:tag>/', views.tag_filter, name='tag_filter'),
+    path('post_tag_detail/<str:tag>/', views.post_tag_detail, name='post_tag_detail'),
+
     #path('like_post/', views.like_post, name='like_post'),
     #path('like_post/<int:post_id>/<slug:slug>/', views.like_post, name='like_post'),
 ]
--- content_management/views.py
+++ content_management/views.py
@@ -8,24 +8,52 @@ from .models import Post
 # Create your views here.
 class PostListView(generic.ListView):
     template_name = "content_management/index.html"
-    queryset = Post.objects.filter(status=1)
+    posts = Post.objects.filter(status=1)
     context_object_name = 'posts'
     model = Post    
-
-#def home(request):
-    #return PostList.as_view()(request)
 
 def post_detail(request, slug):
 
     """
+    Filter for status and slug and display on post_detail template
     """
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
+    posts = Post.objects.filter(status=1)
+    post = get_object_or_404(Post, slug=slug)
 
     return render(
         request,
         "content_management/post_detail.html",
         {"post": post},
     )
+
+
+def tag_filter(request, tag):
+
+    """
+    Filter for tag and display on template
+    """
+    posts = Post.objects.filter(tag=tag, status=1)
+    context = {
+        'tag': tag,
+        'posts': posts
+    }
+
+    return render(request, 'content_management/restaurants_bars.html', context)
+
+def post_tag_detail(request, tag):
+
+    """
+    Filter for status and tag and display on post_detail template
+    """
+    posts = Post.objects.filter(tag=tag, status=1)
+
+    post = posts.first()
+
+    return render(
+        request,
+        "content_management/post_detail.html",
+        {"post": post, "tag": tag},
+    )
+
 
  
