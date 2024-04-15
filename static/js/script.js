7735ec385167bd002c15f46bd99677e0b7ed2a45
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-04-15 12:55:23 +0000

    Add JS to profile-form submit button t change colour after clicked

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('update-profile-btn').addEventListener('click', function () {
+        this.style.backgroundColor = "red";
+    });
+});
