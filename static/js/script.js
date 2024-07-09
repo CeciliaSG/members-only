ab4dba12f89ee9a54e4e32998651aac07ecdc007
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-07-09 08:21:39 +0000

    Change version of bootstrap css

--- static/js/script.js
+++ static/js/script.js
@@ -59,7 +59,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
 const editButtons = document.getElementsByClassName("btn-edit");
 const commentText = document.getElementById("id_body");
 const commentForm = document.getElementById("commentForm");
@@ -86,14 +85,12 @@ for (let button of editButtons) {
     });
 }
 
-/** Delete comments */
-const deleteModal = new bootstrap.Modal(document.getElementById("deleteCommentsModal"));
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deleteButtons = document.getElementsByClassName("btn-delete");
-const deleteConfirm = document.getElementById("deleteCommentConfirm");
+const deleteConfirm = document.getElementById("deleteConfirm");
 
-/**
- * From Blog walkthrough.
- * Initialises deletion functionality for the provided delete buttons.
+/*
+ * Initializes deletion functionality for the provided delete buttons.
  * 
  * For each button in the `deleteButtons` collection:
  * - Retrieves the associated comment's ID upon click.
@@ -102,11 +99,16 @@ const deleteConfirm = document.getElementById("deleteCommentConfirm");
  * - Displays a confirmation modal (`deleteModal`) to prompt 
  * the user for confirmation before deletion.
  */
+document.addEventListener("DOMContentLoaded", () => {
+    const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+    const deleteButtons = document.getElementsByClassName("btn-delete");
+    const deleteConfirm = document.getElementById("deleteConfirm");
 
-for (let button of deleteButtons) {
-    button.addEventListener("click", (e) => {
-        let commentId = e.target.getAttribute("comment_id");
-        deleteConfirm.href = `delete_comment/${commentId}`;
-        deleteModal.show();
-    });
-}
+    for (let button of deleteButtons) {
+        button.addEventListener("click", (e) => {
+            let commentId = e.target.getAttribute("data-comment_id");
+            deleteConfirm.href = `delete_comment/${commentId}`;
+            deleteModal.show();
+        });
+    }
+});
