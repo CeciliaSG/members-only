2b8cd7f28088b47f73c34e763714072d1946e26c
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-07-16 08:51:18 +0000

    Add view, url and template for custom resend_verfication_email

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import edit_user_profile, register, delete_account
+from .views import ( edit_user_profile, register, delete_account, resend_verification_email)
 
 
 urlpatterns = [
     path('core/', register, name='registration'),
     path('account/', edit_user_profile, name='profile'),
     path('delete-account/', delete_account, name='delete_account'),
+    path('resend-verification/', resend_verification_email,
+    name='resend_verification_email'),
 ]
