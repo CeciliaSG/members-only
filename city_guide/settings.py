2a286bee84f94bc86d59c020ad706ae8e0d33140
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-07-18 14:54:43 +0000

    Remove code from settings.py

--- city_guide/settings.py
+++ city_guide/settings.py
@@ -16,8 +16,6 @@ from pathlib import Path
 import os
 import dj_database_url
 
-if os.path.isfile('env.py'):
-    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = [
