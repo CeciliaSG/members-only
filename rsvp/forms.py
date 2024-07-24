c2cabf0bd90450a8d159acdef3d4c1b75add5cee
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-07-24 14:50:15 +0000

    Change rsvp-form

--- rsvp/forms.py
+++ rsvp/forms.py
@@ -5,10 +5,9 @@ from django.contrib.auth.models import User
 class RsvpForm(forms.Form):
     event_id = forms.IntegerField(widget=forms.HiddenInput())
     response = forms.ChoiceField(choices=[(
-        'Yes', 'Yes, of course I will be there.'),
-        ('No', 'No, unfortunately I will not be attending this time.')],
+        'Yes', 'Yes, of course I will be there.')],
      widget=forms.RadioSelect, label="Let us know:")
     num_guests = forms.ChoiceField(choices=[
         ('0', '0'), ('1', '1'), ('2', '2'), ('3', '3'),
                     ('4', '4'), ('5', '5'), ('6', '6')], required=True,
-        label="Bringing a guest. Let us know how many")
+        label="Bringing a guest, or guests. Let us know how many (you + ?)")
